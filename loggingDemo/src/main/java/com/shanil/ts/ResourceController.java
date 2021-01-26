81dd40aa99bf8d39bdfc4b5b1b119373327e7d1a
Shanil TS <shanil.ts@ecstvm.local>
2021-01-26 22:10:16 +0530

    init commit

---
+++ loggingDemo/src/main/java/com/shanil/ts/ResourceController.java
@@ -0,0 +1,38 @@
+package com.shanil.ts;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class ResourceController {
+	@RequestMapping("/home")
+	public String home() throws FileNotFoundException, IOException, XmlPullParserException {
+//		MavenXpp3Reader reader = new MavenXpp3Reader();
+//		Model model = reader.read(new FileReader("pom.xml"));
+//		log.info("CEF:1|ECSFIN|" + model.getArtifactId() + "|" + model.getVersion() + "|DEBUG"
+//				+ "|LoggingRestController" + "|LOW| msg=Testing out logging ");
+		log.info("accessed info");
+		log.warn("accessed warn");
+		log.error("accessed error");
+		log.debug("accessed debug");
+		log.trace("accessed trace");
+		return "home";
+
+	}
+
+	@RequestMapping("/executiontime")
+	public String defaultEntryPoint() {
+		return "executiont time in log";
+
+	}
+}
